a0b0ac783573886f74ec0b2e9a84f2aa2673ca8d
Ivan Rainbolt <7623933+IvanRainbolt@users.noreply.github.com>
2020-09-28 15:51:11 -0400

    Add project files.

---
+++ UI.CLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UI.CLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ UI.FLib/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace UI.FLib
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
